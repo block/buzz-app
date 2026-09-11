8d9dbe7a73e795a80884ccb7916b6e64224a1c5a
Taylor Ho <taylorkmho@gmail.com>
2026-09-11 11:43:40 -1000

    fix(channels): simplify unread navigation hierarchy
    Co-authored-by: Rizz <302abe414ca6e3134763d2539bfcf145aea2a63fe5f8455204ed602fd40cf381@buzz.block.builderlab.xyz>
    Signed-off-by: Taylor Ho <taylorkmho@gmail.com>

--- src/bundled/channels/ChannelsPage.tsx
+++ src/bundled/channels/ChannelsPage.tsx
@@ -364,7 +364,6 @@ function ChannelWorkspace({
                           session={queries}
                           channelId={channel.id}
                           dm={channel.channelType === "dm"}
-                          dmParticipantId={channel.participants?.[0]}
                         />
                       </button>
                     }
